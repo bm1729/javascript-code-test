178baccbbe61f03e2238d4f03207e3071c228ccd
bm1729 <ben.mumford1729@gmail.com>
2025-06-09 22:20:47 +0100

    running tests

---
+++ src/foo.test.ts
@@ -0,0 +1,15 @@
+import { upperCase } from './foo';
+
+describe('foo', () => {
+  it('should return true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should return false', () => {
+    expect(false).toBe(false);
+  });
+
+  it('uppercases a string', () => {
+    expect(upperCase('hello world')).toBe('HELLO WORLD');
+  });
+});
---
+++ src/foo.ts
@@ -0,0 +1,5 @@
+const upperCase = (str: string): string => {
+  return str.toUpperCase();
+}
+
+export { upperCase };
